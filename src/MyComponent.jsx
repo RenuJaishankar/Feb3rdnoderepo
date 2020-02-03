543d4d74f3073787af95818209c25761c0cdcf56
RenuJaishankar <Renu.Jaishankar@yahoo.com>
2020-02-03 15:27:35 -0500

    node commit

---
+++ src/MyComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+const MyComponent = () => {
+    let myname = "Renu"
+    let myhobby = "I really like to code"
+
+    return(
+        <div>
+        <p>my name is{myname} my hobby is {myhobby}</p>
+        </div>
+    )
+}
+ export default MyComponent
